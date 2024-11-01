c2e0a57e24dad474719a90dda402294a478c93a4
Chinonso Nwakudu <nwakudue@gmail.com>
2024-11-01 21:12:46 +0300

    Included an expense tracking by category

--- BudgetTracker.py
+++ BudgetTracker.py
@@ -1,13 +1,22 @@
 
 def set_budget(budget):
     total_expenses = 0
+    category_totals = {}
     
     
     def expense_adding(amount, item):
         nonlocal total_expenses
+        nonlocal category_totals
         
         total_expenses = 50
         total_expenses += amount
+        
+        if item in category_totals:
+            category_totals[item] +=amount
+        else:
+            category_totals[item] = amount
+
+        print("category total is" + str(category_totals))    
         
         print("\n" "Added " + item + " which cost " + str(amount)+ " and gave a total of " + str(total_expenses))
         
